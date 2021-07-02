5e2c6bc21d64ea9587171a9e115372c892c91fac
Shawlycode <kutinaorlando@gmail.com>
2021-07-02 11:47:48 +0000

    returned components in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import List from "./List";
 import './App.css'
 
 function App() {
+  const [people, setPeople] = useState(Data);
   return (
-    <div className="App">
-      <Data />
-      <List />
-    </div>
+   <main>
+     <section className="container">
+       <h3>{people.length } birthdays today</h3>
+       <List people={people}/>
+       <button onClick={()=>setPeople([])}>
+         Clear All
+       </button>
+     </section>
+   </main>
   );
-}
+};
 
-export default App;
+export default App
